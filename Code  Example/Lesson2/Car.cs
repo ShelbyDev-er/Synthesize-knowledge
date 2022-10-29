e48742ed9ea4bd118669947b2793e8df82582266
Eric Bui <eric.bui@ansarada.com>
2022-10-29 22:39:07 +0700

    Add code example

---
+++ Code  Example/Lesson2/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+	public class Vehicle
+	{
+		// Attribute
+		private int height;
+		public int weight;
+		public int lenght;
+		public string name;
+
+		public Vehicle(string vehicleName)
+		{
+			Console.WriteLine($"{vehicleName} is created !!!");
+		}
+
+		public void SetHeight(int height)
+		{
+			if (height < 0)
+			{
+				this.height = 0;
+			}
+			else
+			{
+				this.height = height;
+			}
+		}
+
+		public void SetHeight(decimal height)
+		{
+			if (height < 0)
+			{
+				this.height = 0;
+			}
+			else
+			{
+				this.height = (int)height;
+			}
+		}
+
+		public void SetHeight(int height, int tmp)
+		{
+			if (height < 0)
+			{
+				this.height = 0;
+			}
+			else
+			{
+				this.height = (int)height;
+			}
+		}
+
+		public int GetHeight()
+		{
+			return this.height;
+		}
+	}
+
+	public class Car : Vehicle
+	{
+		// Constructor
+		public Car(string carName) : base(carName)
+		{
+			Console.WriteLine($"Car is created !!!");
+		}
+	}
+
+	public class Motobike : Vehicle
+	{
+		// Constructor
+		public Motobike(string motobikeName) : base(motobikeName)
+		{
+		}
+	}
+}
---
+++ Code  Example/Lesson2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lesson2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Phan biet, hieu va biet cach su dung cac kieu du lieu tap hop (danh sach) trong c#
+			// Cu phap
+			// Cac phuong thuc ma kieu du lieu ho tro(CRUD, search, sort)
+			// Test
+
+			// 4 thuoc tinh quan trong cua OOP
+			// Tinh dong goi (Encapsulation)
+			/*
+			    Access Modifier:
+					- public
+					- private
+					- protected
+					- sealed
+			*/
+
+
+			// Tinh ke thua (Inheritance)
+
+
+			// Tinh da hinh (Polymorphism)
+
+			// Tinh truu tuong (Abstraction)
+
+			// Override(ghi de) vs Overloading(nap chong ham)
+
+			Vehicle car1 = new Car("BWM");
+			Vehicle motobike2 = new Motobike("Honda");
+			Car car3 = (Car)car1;
+			Motobike motobike = (Motobike)motobike2;
+
+			car1.SetHeight(10);
+			motobike2.SetHeight(20);
+
+			Console.WriteLine($"height of car1 is: {car1.GetHeight()}");
+
+			car1.SetHeight(1);
+			car1.SetHeight((decimal)10.5);
+			car1.SetHeight(5, 6);
+		}
+	}
+}
