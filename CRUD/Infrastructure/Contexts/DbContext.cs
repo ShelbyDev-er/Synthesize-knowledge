f51ddbad6f36d85d4f53d5729ef96e0846956f11
Eric Bui <eric.bui@ansarada.com>
2022-11-19 16:06:14 +0700

    Initial EF  Core

---
+++ CRUD/Infrastructure/Contexts/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+    public class PodStoreContext : DbContext
+    {
+        public DbSet<PodDataModel> Pods { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
